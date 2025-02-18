f1148c26e5d3f6d5a83f7b5abe5e07dc2a8e670c
tracyingua <qb202100548@wmsu.edu.ph>
2025-02-19 01:05:54 +0800

    about-us description update

--- src/components/about-us.jsx
+++ src/components/about-us.jsx
@@ -13,7 +13,7 @@ export default function navigation() {
                 </div>
                 <p className="aboutMe">Hi there! I’m Tracy Anne, a passionate Graphic Designer, UI/UX Designer, and Photographer dedicated to crafting intuitive and engaging digital experiences. With a keen eye for aesthetics and a deep understanding of user needs, I specialize in creating seamless interfaces that not only enhance usability but also bring brands to life through thoughtful design.
 
-My journey in design is fueled by creativity, attention to detail, and a constant drive to explore new ideas. Whether it’s designing user-centric digital solutions, developing unique branding, or capturing life’s special moments through photography, I strive to deliver work that resonates and inspires.
+My journey in the world of design is fueled by creativity, attention to detail, and a constant drive to explore new ideas. Whether it’s designing user-centric digital solutions, developing unique branding, or capturing life’s special moments through photography, I strive to deliver work that resonates and inspires.
 
 I believe that great design goes beyond visuals—it tells a story, evokes emotions, and solves problems. Let’s work together to transform your vision into captivating designs and unforgettable experiences!</p>
 

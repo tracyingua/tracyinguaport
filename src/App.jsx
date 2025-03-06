1617688f6e0b593374c95560afb75b87ff4c14d0
tracyingua <qb202100548@wmsu.edu.ph>
2025-03-06 14:28:10 +0800

    my newly improved design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import Hero from "./components/hero"
-import About from "./components/about-us"
+
+import Header from "./components/header"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Hero from "./components/hero"
+
+import About from "./components/about-us"
+
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/about" element={<About />} />
--- src/components/button-me.jsx
+++
@@ -1,6 +0,0 @@
-import "../assets/styles/button.css"
-
-
-export default function button({title }) {
-    return <button className="button">{title}</button>
-}
